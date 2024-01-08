f0c49d01e064eaf6631ead4e30826b1020ee91ec
chloe0524 <chloco31@gmail.com>
2024-01-08 13:13:12 -0800

    issue name of function

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -16,6 +16,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	func(tree->n);
-	binary_tree_inorder(tree->left, func);
-	binary_tree_inorder(tree->right, func);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
