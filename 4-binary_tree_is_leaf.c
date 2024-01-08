60b19b93de96505644e5c733116bf7f39435a73f
chloe0524 <chloco31@gmail.com>
2024-01-08 05:02:33 -0800

    is leaf better

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,8 +9,8 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return;
+		return (0);
 	if (node->left == NULL && node->right == NULL)
-		return (2);
+		return (1);
 	return (0);
 }
