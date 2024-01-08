145bf9f7d7087693c86164510801feef55cb38fd
chloe0524 <chloco31@gmail.com>
2024-01-08 13:10:30 -0800

    mixed up two file content

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -15,7 +15,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (func == NULL)
 		return;
 
-	binary_tree_inorder(tree->left, func);
 	func(tree->n);
+	binary_tree_inorder(tree->left, func);
 	binary_tree_inorder(tree->right, func);
 }
