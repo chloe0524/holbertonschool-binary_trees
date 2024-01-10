568f563c4ad48b4833347135503d96c36f7fbe48
chloe0524 <chloco31@gmail.com>
2024-01-10 02:04:24 -0800

    forgot removing +1 after moved it somewhere else

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -14,9 +14,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL)
-		hleft = binary_tree_height(tree->left) + 1;
+		hleft = binary_tree_height(tree->left);
 	if (tree->right != NULL)
-		hright = binary_tree_height(tree->right) + 1;
+		hright = binary_tree_height(tree->right);
 
 	return (hleft >= hright ? (hleft + 1) : (hright + 1));
 }
