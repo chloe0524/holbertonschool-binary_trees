daddc9ef81d52a296c850516ddff1e3bf5072ffe
chloe0524 <chloco31@gmail.com>
2024-01-10 09:48:39 -0800

    modified addition

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,9 +13,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
-
-	return (left_leaves + right_leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
 
