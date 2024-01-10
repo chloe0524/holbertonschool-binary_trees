68bd312ff79fd4e22c485f5f813c59535c2a06ef
Yass <135179961+Fiegsy@users.noreply.github.com>
2024-01-10 14:38:32 +0100

    Update 17-binary_tree_sibling.c by adding return "NULL)

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,6 +8,7 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
     if (node == NULL || node->parent == NULL)
+        return (NULL);
 
     if (node == node->parent->left)
         return (node->parent->right);
