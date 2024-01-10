5b2903a95c0360e39d821126f49978e7c192b6c5
chloe0524 <chloco31@gmail.com>
2024-01-10 01:40:00 -0800

    added previous task 9 height

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,5 +1,28 @@
 #include "binary_trees.h"
 
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of
+ * a binary tree
+ * @tree: pointer to tree
+ * Return: height of tree or 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t hleft = 0, hright = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		hleft = binary_tree_height(tree->left) + 1;
+	if (tree->right != NULL)
+		hright = binary_tree_height(tree->right) + 1;
+
+	return (hleft >= hright ? (hleft + 1) : (hright + 1));
+}
+
 /**
  * binary_tree_levelorder - goes through a binary tree
  * using level-order traversal
