0613c09544d5ad4c13d0c5d748d6816e747e489a
chloe0524 <chloco31@gmail.com>
2024-01-10 01:26:27 -0800

    last change: fixed issue with function name in header file

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - goes through a binary tree
+ * using level-order traversal
+ *
+ * @tree: pointer to the root node of the tree
+ * @func: pointer to a function to call for each node
+ *
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+
+	if (tree != NULL)
+	{
+		binary_tree_levelorder(tree->left, func);
+		binary_tree_levelorder(tree->right, func);
+	}
+}
