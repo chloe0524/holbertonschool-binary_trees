b5dc3e20bda2b62e58773aae9159d218cb9ff97d
chloe0524 <127857895+chloe0524@users.noreply.github.com>
2024-01-10 21:06:39 +0100

    Update 12-binary_tree_leaves.c return to previous code

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,15 +7,13 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
-
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	return (left_leaves + right_leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
+
 
